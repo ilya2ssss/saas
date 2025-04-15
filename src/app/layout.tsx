79331ef8c18052bc8256e5ce88584f4019b141a1
ilya2s <ilyass.elouazzani97@outlook.fr>
2025-04-15 17:43:31 -0400

    Enhance layout structure and introduce Prettier configuration
    - Added a .prettierrc file to enforce consistent code formatting across the project.
    - Updated the RootLayout component to include SidebarInset for improved layout management.
    - Refactored AppSidebar to enhance navigation links and ensure proper routing for portfolio items.
    - Improved code readability by aligning indentation and formatting in layout.tsx and app-sidebar.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,45 +2,44 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/layout/app-sidebar";
 
 const inter = Inter({
-  variable: "--font-inter-sans",
-  subsets: ["latin"],
+    variable: "--font-inter-sans",
+    subsets: ["latin"],
 });
 
 // TODO: Complete metadata
 export const metadata: Metadata = {
-  title: "Cora · AI management workers for your properties",
-  description: "AI at the service of property managers, ...",
-  authors: [{ name: "...", url: "..." }],
-  keywords: [],
-  robots: "",
-  category: "technology",
+    title: "Cora · AI management workers for your properties",
+    description: "AI at the service of property managers, ...",
+    authors: [{ name: "...", url: "..." }],
+    keywords: [],
+    robots: "",
+    category: "technology",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable,
-        )}
-      >
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    inter.variable
+                )}
+            >
+                <SidebarProvider>
+                    <AppSidebar />
+                    <SidebarInset>
+                        <main>{children}</main>
+                    </SidebarInset>
+                </SidebarProvider>
+            </body>
+        </html>
+    );
 }
--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,87 +1,93 @@
 import {
-  Briefcase,
-  Building,
-  DoorOpen,
-  FileText,
-  LayoutDashboard,
-} from "lucide-react";
+    Briefcase,
+    Building,
+    DoorOpen,
+    FileText,
+    LayoutDashboard,
+} from 'lucide-react'
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "../ui/sidebar";
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '../ui/sidebar'
 
-// Portfolio menu portfolioItems
-// TODO: Add Dashboard
 const portfolioItems = [
-  {
-    title: "Owners",
-    url: "#",
-    icon: Briefcase,
-  },
-  {
-    title: "Properties",
-    url: "#",
-    icon: Building,
-  },
-  {
-    title: "Units",
-    url: "#",
-    icon: DoorOpen,
-  },
-  {
-    title: "Leases",
-    url: "#",
-    icon: FileText,
-  },
-];
+    {
+        title: 'Owners',
+        url: '/owners',
+        icon: Briefcase,
+    },
+    {
+        title: 'Properties',
+        url: 'properties',
+        icon: Building,
+    },
+    {
+        title: 'Units',
+        url: 'units',
+        icon: DoorOpen,
+    },
+    {
+        title: 'Leases',
+        url: 'leases',
+        icon: FileText,
+    },
+]
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader />
+    return (
+        <Sidebar variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem key="Cora AI">
+                        <SidebarMenuButton asChild>
+                            <span>LOGO PLACEHOLDER</span>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
 
-      <SidebarContent>
-        {/* Dashboard */}
-        <SidebarMenu>
-          <SidebarMenuItem key="Dashboard">
-            <SidebarMenuButton asChild>
-              <a href="#">
-                <LayoutDashboard />
-                <span>Dashboard</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
+            <SidebarContent>
+                {/* Dashboard */}
+                <SidebarMenu>
+                    <SidebarMenuItem key="Dashboard">
+                        <SidebarMenuButton asChild>
+                            <a href="/dashboard">
+                                <LayoutDashboard />
+                                <span>Dashboard</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
 
-        {/* Portfolio */}
-        <SidebarGroup>
-          <SidebarGroupLabel>Portfolio</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {portfolioItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+                {/* Portfolio */}
+                <SidebarGroup>
+                    <SidebarGroupLabel>Portfolio</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {portfolioItems.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
 
-      <SidebarFooter />
-    </Sidebar>
-  );
+            <SidebarFooter />
+        </Sidebar>
+    )
 }
